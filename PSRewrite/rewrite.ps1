87805236c46d6d6260074ce3ca27ff2528a6cb92
Will <willchan33@gmail.com>
2018-06-30 03:37:01 -0400

    new ps

--- PSRewrite/rewrite.ps1
+++ PSRewrite/rewrite.ps1
@@ -26,27 +26,24 @@ function Set-OrAddIniValue
 <#--makes customer filedirecotry--#>
 function createDirectory($cName){
     New-Variable -Name dir1 -Value "C:\Users\WCHAN\Desktop\PSRewrite\$cName" -Scope Global
-    New-Item -Path $dir1 -ItemType Directory -Force
-    cp -Force -Recurse C:\Users\WCHAN\Desktop\PSRewrite\clean\* $dir1
-    
+    New-Item -Path $dir1 -ItemType Directory -Force 
+    cp -Force -Recurse C:\Users\WCHAN\Desktop\PSRewrite\clean\* $dir1 
     
 }
 
 
 <#---cleanfiles #>
-$list = @()
-$list += "\web\iceconfig.js"
-$list +=  "\web\Web.config"
-$list += "\packager\install.ini"
-$list += "\recorder\iceRecordingManager.exe.config"
-$list += "\recorder\iceUCMARecorder.exe.config"
-$list += "\imr\iceIMRHostService.exe.config"
+$varList = Get-ChildItem *.* -Recurse | Select-Object FullName 
 
 
 <#---vars---#>
 $cName = "BCMSD"
 $webServer = "web12.icescape.com"
+$iceServer = "bcmsdtest.icescape.com"
 $switchID = "11001"
+$imrPort = "9500"
+$netTcpPort = "10000"
+$sRPort= "9600"
 $sqlServer = "ctdvmsql01.ctd.local"
 $switchDB = "ctdvmice01Switch"+$switchID
 $sqlUser = "sa"
@@ -91,9 +88,45 @@ $fList = @()
 <#--actions--#>
 createDirectory($cName) --quiet
 
-<#
-for($i = 0; $i -lt $list.Count; $i++)
+
+for($i = 0; $i -lt $varList.Count; $i++)
 {
-   $fList += $dir1 + $list[$i]
-   foreach
-}#>
+   $item1 =  $varList[$i].FullName
+   #Write-Host $item1
+   #$FCV = Get-Content -Path $item1 
+   
+   switch ($item1){
+
+   {$item1 -like '*iceIMR*'} { 
+   Write-Host "IMR!!!"
+   $FileContentVariable = Get-Content -Path $item1 | ForEach-Object {
+   #$_.replace("https://localhost:8301/iceIMRService", "https://$($webServer):$($imrPort)/iceIMRService").replace("net.tcp://localhost:8401/iceIMRService", "net.tcp://$($webServer):$($netTcpPort)/iceIMRService").replace("<add key=""iceIMRService.SwitchId"" value=""11001""/>", "<add key=""iceIMRService.SwitchId"" value=""$($switchID)""/>").replace("<add key=""iceIMRService.icePassword"" value=""PASSWORD""/>", "<add key=""iceIMRService.icePassword"" value=""288""/>").replace("<add key=""iceIMRService.iceAddress"" value=""ice.domain.com""/>", "<add key=""iceIMRService.iceAddress"" value=""$($iceServer)""/>")  
+   
+    $_.replace("https://localhost:8301/iceIMRService", "https://$($webServer):$($imrPort)/iceIMRService").replace("net.tcp://localhost:8401/iceIMRService", "net.tcp://$($webServer):$($netTcpPort)/iceIMRService").replace("<add key=""iceIMRService.SwitchId"" value=""11001""/>", "<add key=""iceIMRService.SwitchId"" value=""$($switchID)""/>").replace("<add key=""iceIMRService.icePassword"" value=""PASSWORD""/>", "<add key=""iceIMRService.icePassword"" value=""288""/>").replace("<add key=""iceIMRService.iceAddress"" value=""ice.domain.com""/>", "<add key=""iceIMRService.iceAddress"" value=""$($iceServer)""/>").replace("<add key=""iceIMRService.EmailAlertSMTPServer"" value=""email.domain.com""/>", "<add key=""iceIMRService.EmailAlertSMTPServer"" value=""10.258.258.25""/>").replace("<add key=""iceIMRService.iceDBConnectionString"" value=""Data Source=""$($sqlServer)"";Initial Catalog=iceSwitch11001;Persist Security Info=True;User Id=sa;Password=PASSWORD""/>","<add key=""iceIMRService.iceDBConnectionString"" value=""Data Source=""$($sqlServer)"";Initial Catalog=""$($iceDB)"";Persist Security Info=True;User Id=sa;Password=288""/>").replace("<add key=""iceIMRService.iceMailDBConnectionString"" value=""Data Source=localhost;Initial Catalog=iceMail;Persist Security Info=True;User Id=sa;Password=PASSWORD""/>","<add key=""iceIMRService.iceMailDBConnectionString"" value=""Data Source=$($sqlServer);Initial Catalog=$($iceDB)Mail;Persist Security Info=True;User Id=sa;Password=288""/>")
+
+
+   }
+   $FileContentVariable | Out-File -FilePath $item1
+   #Write-Host $FileContentVariable
+   
+   <#
+   $FileContentVariable = $FileContentVariable -replace ("https://localhost:8301/iceIMRService", "https://$($webServer):$($imrPort)/iceIMRService")
+   $FileContentVariable = $FileContentVariable -replace ("net.tcp://localhost:8401/iceIMRService", "net.tcp://$($webServer):$($netTcpPort)/iceIMRService")
+   $FileContentVariable = $FileContentVariable -replace ("<add key=""iceIMRService.SwitchId"" value=""11001""/>", "<add key=""iceIMRService.SwitchId"" value=""$($switchID)""/>")
+  
+   $FileContentVariable | Out-File -FilePath $item1
+   #>
+  }
+
+  <#
+
+   {$item1 -like '*install*'} {Write-Host "install!"   }
+   {$item1 -like '*RecordingManager*'} {Write-Host "RecordingManager!"   }
+   {$item1 -like '*UcmaRec*'} {Write-Host "UCMA!"   }
+   {$item1 -like '*.js*'} {Write-Host "js!"   }
+   {$item1 -like '*web*'} {Write-Host "web!"   }
+   #>
+   }
+
+}
+
